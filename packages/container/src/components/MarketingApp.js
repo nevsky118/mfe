e8faef1e4ec76668fd69250ba5c7820ad2f2bf14
Vadim <nevsky118@gmail.com>
2021-09-05 13:52:16 +0300

    added auth subapp

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,14 +1,25 @@
-import React, { useRef, useEffect } from 'react';
 import { mount } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
-const MarketingApp = () => {
-	const ref = useRef(null);
+export default () => {
+  const ref = useRef(null);
+  const history = useHistory();
 
-	useEffect(() => {
-		mount(ref.current);
-	}, []);
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      initialPath: history.location.pathname,
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
 
-	return <div ref={ref} />;
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
+
+  return <div ref={ref} />;
 };
-
-export default MarketingApp;
